15b3413084d38c10d149aa8b92d67df3b1d9d5ea
GitHubCanada <154619406+GitHubCanada@users.noreply.github.com>
2023-12-22 12:33:46 -0500

    Update streamlit_app.py
    resolve the error of the Header, replace Mom's to Parents

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('My Mom's New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('BreakFast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
